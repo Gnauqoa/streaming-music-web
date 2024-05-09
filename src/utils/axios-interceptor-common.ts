1e8e867ad1dad1504530ca168d27ddaa6b602144
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-05-09 14:41:44 +0700

    Remove platform id.

--- src/utils/axios-interceptor-common.ts
+++ src/utils/axios-interceptor-common.ts
@@ -1,8 +1,6 @@
 import { getToken, saveToken } from "./local-storage";
 import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 
-const ClientID = process.env.API_CLIENT_ID || "binary-options";
-
 const interceptors = {
   request: [
     (config: AxiosRequestConfig) => {
@@ -11,23 +9,6 @@ const interceptors = {
         config = {
           ...config,
           headers: { Authorization: `Bearer ${token}` },
-        };
-      }
-      if (ClientID) {
-        config = {
-          ...config,
-          headers: {
-            "Client-ID": ClientID,
-          },
-        };
-      }
-      if (ClientID && token) {
-        config = {
-          ...config,
-          headers: {
-            "Client-ID": ClientID,
-            Authorization: `Bearer ${token}`,
-          },
         };
       }
 
