9f1d1f6a939754c1b11b1e02789d22d32e46aeba
tdyhoang <tdyhoang@gmail.com>
2024-05-12 10:46:50 +0700

    LogInPage layout

--- src/layout/SignUpPage.tsx
+++ src/layout/SignUpPage.tsx
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom";
+import Logo from "../components/authorization/Logo";
+import SocialLogin from "../components/authorization/SocialLogin";
 import SignUp from "../components/authorization/SignUp";
-import Logo from "../assets/Logo.png";
-import { GoogleLogin } from "@react-oauth/google";
-import "../components/authorization/SignUp.css";
-// import { isLoggedIn } from "../utils";
+import "../components/authorization/Authorization.css";
+// import { isLoggedIn } from "../utils/";
 
 export default function SignUpPage() {
   // return isLoggedIn() ? (
@@ -11,24 +10,10 @@ export default function SignUpPage() {
   // ) : (
   return (
     <div>
-      <div style={{}}>
-        <div className="NavBarStyle">
-          <div className="barsection">
-            <Link to="/">
-              <img src={Logo} className="Logo" alt="" />
-            </Link>
-          </div>
-        </div>
+      <div>
+        <Logo />
         <section>
-          <div className="SocialIconsPage">
-            <h6 className="hint">Sign Up with your social media account</h6>
-            <div className="googlelogin">
-              <GoogleLogin onSuccess={() => {}} onError={() => {}} />
-            </div>
-            <section className="or-seperator OR">
-              <h6 className="hint">Sign up with your email address</h6>
-            </section>
-          </div>
+          <SocialLogin />
           <SignUp />
         </section>
       </div>
