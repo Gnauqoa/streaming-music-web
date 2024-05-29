86f4141fd98449f0b4600cf34657c613308faf9d
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-05-29 22:12:33 +0700

    Add music type.

---
+++ src/@types/music.ts
@@ -0,0 +1,12 @@
+import { Artist } from "./artist";
+
+export type Music = {
+  id: number;
+  name: string;
+  likes_count: number;
+  liked: boolean;
+  source_url: string;
+  created_at: string;
+  updated_at: string;
+  artists: Artist;
+};
